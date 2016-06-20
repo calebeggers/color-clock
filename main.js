fd20a97747feab45a7ab93a37890c10c859a63af
calebeggers <calebeggers11@gmail.com>
2016-06-20 16:00:39 -0400

    Completed work on clock. Added a working pull and a color randomizer.

---
+++ main.js
@@ -0,0 +1,37 @@
+var checkTime = function (i) {
+	if (i < 10) {i = "0" + i};
+	return i;
+}
+
+// var currentTime = new Date ();
+// var m = currentTime.getMinutes();
+// var s = currentTime.getSeconds();
+// var h = currentTime.getHours();
+
+// m = checkTime(m)
+// s = checkTime(s)
+
+var callback = function () {
+	var currentTime = new Date ();
+	var m = currentTime.getMinutes();
+	var s = currentTime.getSeconds();
+	var h = currentTime.getHours();
+	m = checkTime(m)
+	s = checkTime(s)
+	document.getElementById("clock").textContent = h + ":" + m + ":" + s;
+}
+
+var interval = window.setInterval(callback, 1000)
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF".split("");
+    var color = "#";
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+	document.getElementById("clock").style.background = color;
+}
+
+getRandomColor()
+
+var interval2 = window.setInterval(getRandomColor, 2000)
